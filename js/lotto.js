e9836bcc102a7bb5736695b86705fa7303159c1b
bingsoo95 <kimminsu403@naver.com>
2021-11-15 11:52:01 +0900

    1115

--- js/lotto.js
+++ js/lotto.js
@@ -18,11 +18,17 @@ btn.addEventListener("click",(event) => {
     //clear all <h3> tags
     target.innerHTML =''
 
+    let str =''
+
     for (const num of balls) {
-        const tag = document.createElement("h3")
-        const textNode = document.createTextNode(num)
-        tag.appendChild(textNode)
-        target.appendChild(tag)
+
+        str += `<h3>${num}</h3>`
+
+        target.innerHTML = str
+        // const tag = document.createElement("h3")
+        // const textNode = document.createTextNode(num)
+        // tag.appendChild(textNode)
+        // target.appendChild(tag)
     }
 
 
@@ -31,7 +37,28 @@ btn.addEventListener("click",(event) => {
 
 function makeLottoNum(){
 
-    const numArr = [2,13,21,32,35,40]
+    const arr = []
 
-    return numArr
+    for (let i = 0; i < 6; i++) {
+        const tempNum = parseInt(Math.random() * 45) + 1
+        //arr안에 tempNum이 있다면
+        if( arr.indexOf(tempNum) >= 0 ){
+            console.log("중복")
+            i--
+            continue
+        }
+        arr.push(tempNum)
+    }
+
+    // arr.sort(function(a,b) {
+    //     return a -b
+    // })
+
+    arr.sort((a,b) => a - b )
+
+    return arr
+
 }
+
+
+
